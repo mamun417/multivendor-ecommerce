793836bd317d9ba442fe99a092295243f1d5b969
mamun417 <abdullah.ssc13@gmail.com>
2021-05-08 16:06:19 +0600

    attrs tag input submit form issue fix

--- resources/views/admin/pages/products/elements.blade.php
+++ resources/views/admin/pages/products/elements.blade.php
@@ -1,6 +1,5 @@
 @push('style')
     <link href="{{ asset('backend/css/plugins/select2/select2.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('backend/css/plugins/summernote/summernote-bs4.css') }}" rel="stylesheet">
     <link href="{{ asset('backend/css/plugins/bootstrap-tagsinput/bootstrap-tagsinput.css') }}" rel="stylesheet">
 @endpush
 
@@ -160,7 +159,7 @@
                     <div class="col-md-4">
                         <label>Color</label>
                         <div class="input-group">
-                            <input name="color" class="tagsinput form-control" type="text"
+                            <input name="color" class="tagsinput form-control test-me" type="text"
                                    value="{{ $product->attributes->color ?? old('color')}}"/>
                         </div>
                     </div>
@@ -313,6 +312,12 @@
     <script>
         $('.tagsinput').tagsinput({
             tagClass: 'label label-primary',
+        });
+
+        $('.bootstrap-tagsinput input').keypress(function (e) {
+            if (e.keyCode === 13) {
+                e.preventDefault();
+            }
         });
     </script>
 @endpush
